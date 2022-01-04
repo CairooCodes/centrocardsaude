74c5dd7cbc65812471bc17521a00bb81e41cd41f
cairofelipedev <cairofelipedev@gmail.com>
2022-01-03 22:36:28 -0300

    new style navbar

--- index.php
+++ index.php
@@ -7,39 +7,84 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <title>Hello, world!</title>
 </head>
 <style>
   .nav-main {
     box-shadow: 1px 1px 1px hsl(0, 2%, 84%);
   }
+
   .brand {
-    color:rgb(180, 0, 0);
-    font-size:22px;
+    color: #E70006;
+    font-size: 22px;
     font-weight: bold;
   }
+
   .nav-link {
-    color:gray;
+    color: black;
     font-weight: 500;
   }
+
   .btn-nav {
-    background-color:rgb(180, 0, 0);
-    padding:5px 30px 5px 30px;
-    color:white;
-    border-radius:20px;
+    background-color: #E70006;
+    padding: 5px 30px 5px 30px;
+    color: white;
+    border-radius: 20px;
   }
+
   i {
-    font-size:28px;
+    font-size: 28px;
+  }
+
+  .dropdown:hover .dropdown-menu {
+    display: block;
+    margin-top: 0;
+  }
+
+  .form {
+    position: relative
+  }
+
+
+
+  .form span {
+    position: absolute;
+    right: 17px;
+    top: 7px;
+    border-left: 1px solid #d1d5db
+  }
+
+  .left-pan {
+    padding-left: 7px
+  }
+
+  .left-pan i {
+    padding-left: 10px
+  }
+
+  .form-input {
+    height: 40px;
+    border-radius: 50px
+  }
+
+  .form-input:focus {
+    box-shadow: none;
+    border: none
+  }
+  .btn-login {
+    background-color: transparent;
+    border-color:transparent
   }
 </style>
+
 <body>
   <nav class="nav-main">
     <div class="container">
       <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3">
-        <a href="/" class="brand d-flex align-items-center col-md-3 mb-2 mb-md-0 text-decoration-none">
+        <a href="/" class="brand d-flex align-items-center mb-2 mb-md-0 text-decoration-none">
           CENTROCARD
         </a>
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
@@ -54,8 +99,7 @@
           <button type="button" class="btn btn-nav">Afiliado</button>
         </div>
         <div class="dropdown text-end">
-          <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1"
-            data-bs-toggle="dropdown" aria-expanded="false">
+          <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
             <i class="bi bi-person-circle"></i>
           </a>
           <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
@@ -71,24 +115,57 @@
       </header>
     </div>
   </nav>
-  <section  style="background-color:#B32929">
-  <div class="container col-xxl-8">
-    <div class="row flex-lg-row-reverse align-items-center">
-      <div class="col-10 col-sm-8 col-lg-6">
-        <img src="mockup-semfundo.png" class="d-block mx-lg-auto img-fluid" loading="lazy">
-      </div>
-      <div class="col-lg-6">
-        <h1 class="display-5 fw-bold lh-1 mb-3">Responsive left-aligned hero with image</h1>
-        <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most
-          popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive
-          prebuilt components, and powerful JavaScript plugins.</p>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-          <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+  <header class="p-3">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="brand d-flex align-items-center mb-2 mb-md-0 text-decoration-none">
+          CENTROCARD
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="#" class="nav-link px-2">Home</a></li>
+          <li><a href="#" class="nav-link px-2">Features</a></li>
+          <li><a href="#" class="nav-link px-2">Pricing</a></li>
+          <li><a href="#" class="nav-link px-2">FAQs</a></li>
+          <li><a href="#" class="nav-link px-2">About</a></li>
+        </ul>
+
+        <!-- <div class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3 d-flex">
+          <div class="form">
+            <input type="text" class="form-control form-input" placeholder="Produtos, serviços...">
+            <span class="left-pan"><i class="fa fa-search"></i> </span>
+          </div>
+        </div> -->
+
+        <div class="text-end">
+          <button type="button" class="btn btn-nav">Afiliado</button>
+          <button class="btn-login" type="button" class="btn">
+          <div class="d-flex align-items-center">
+          Login <i class="bi bi-box-arrow-in-right"></i>
+          </div>
+        </button>
         </div>
       </div>
     </div>
-  </div>
+  </header>
+  <section style="background-color:#E70006">
+    <div class="container col-xxl-8">
+      <div class="row flex-lg-row-reverse align-items-center">
+        <div class="col-10 col-sm-8 col-lg-6">
+          <img src="mockup-semfundo.png" class="d-block mx-lg-auto img-fluid" loading="lazy">
+        </div>
+        <div class="col-lg-6">
+          <h1 class="display-5 fw-bold lh-1 mb-3">Responsive left-aligned hero with image</h1>
+          <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most
+            popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive
+            prebuilt components, and powerful JavaScript plugins.</p>
+          <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+            <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+            <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
   <div class="container">
     <footer class="py-5">
@@ -158,9 +235,7 @@
   <!-- Optional JavaScript; choose one of the two! -->
 
   <!-- Option 1: Bootstrap Bundle with Popper -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
   <!-- Option 2: Separate Popper and Bootstrap JS -->
   <!--
